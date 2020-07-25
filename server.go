d8fcacaef74370254241e7a4f3129a723b92afd5
negibokken <negibokken@gmail.com>
2020-07-25 09:37:52 +0900

    Add simple echo

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package httpserver
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type HTTPServer interface {
@@ -11,10 +13,21 @@ type HTTPServer interface {
 
 type HTTPServerListenError error
 
+func handleConnection(conn net.Conn) {
+	io.Copy(os.Stdout, conn)
+}
+
 func NewServer() (HTTPServer, error) {
 	server, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		return nil, fmt.Errorf("http server listen error, %v", err)
+	}
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			return nil, fmt.Errorf("http server accept error, %v", err)
+		}
+		go handleConnection(conn)
 	}
 	return server, nil
 }
